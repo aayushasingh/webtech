66f4f3cd14b28f7108381e249a0cbcf57e7f5816
aayushasingh <119467460+aayushasingh@users.noreply.github.com>
2023-01-25 08:27:46 +0545

    week 9 added

---
+++ week 9/js/script.js
@@ -0,0 +1,18 @@
+alert("dont make noise in class");
+console.log("hello from the other side ");
+console.log(document.getElementById("paragraph").innerHTML)
+ document.getElementById("paragraph").innerHTML="<h1>Hello world!</h1>";
+// document.getElementById("paragraph").style.color="green";
+document.getElementById("paragraph").style.cssText="color:purple;background=aqua";
+var age =10;
+age=13;
+const age1 =10;
+//age1=90;
+let age3=10;
+age3=20;
+var name="aayusha";
+
+console.log(age);
+console.log(age1);
+console.log(typeof age3);
+
